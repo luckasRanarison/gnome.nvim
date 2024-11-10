db07b232b6200c62d6a674cd61f772df26568ab1
luckasRanarison <luckasranarison@gmail.com>
2024-11-10 19:27:38 +0300

    refactor: cleanup

--- lua/gnome/init.lua
+++ lua/gnome/init.lua
@@ -1,10 +1,10 @@
 local M = {}
 
-local config = require("gnome.config")
-local usercmd = require("gnome.usercmd")
-local notify = require("gnome.notify")
 local utils = require("gnome.utils")
 local color = require("gnome.color")
+local notify = require("gnome.notify")
+local config = require("gnome.config")
+local usercmd = require("gnome.usercmd")
 
 ---@param options Gnome.Option
 M.setup = function(options)
--- lua/gnome/utils.lua
+++ lua/gnome/utils.lua
@@ -3,7 +3,6 @@ local M = {}
 M.get_gjs_dir = function()
   local lua_script_path = debug.getinfo(1, "S").source:sub(2)
   local lua_script_dir = vim.fn.fnamemodify(lua_script_path, ":p:h")
-
   return lua_script_dir .. "/../../gjs"
 end
 
